5268a32dd8774b183ca64ed0e3da22f14db2f34b
范春晨 <305685854@qq.com>
2021-01-18 15:46:09 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view />
+    <van-button>按钮</van-button>
+  </div>
+</template>
+
+<style lang="less">
+html,
+body,
+#app {
+  height: 100%;
+  background-color: #efefef;
+}
+</style>
